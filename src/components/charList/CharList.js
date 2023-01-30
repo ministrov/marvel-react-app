f902f8478df84d80f01371b4c6a2a71ea5ff841b
ministrov <61511030+ministrov@users.noreply.github.com>
2023-01-30 21:20:27 +0300

    feat: add ProrType check to Charlist and CharInfo component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+import PropTypes from 'prop-types';
 class CharList extends Component {
     state = {
       charList: [],
@@ -108,6 +109,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
