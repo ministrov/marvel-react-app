06beaa279ebea507b5c0ed1893aa037639dac257
ministrov <61511030+ministrov@users.noreply.github.com>
2023-06-19 18:15:11 +0300

    feat: create CharSearchForm component to find a one character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,6 +12,11 @@ const useMarvelService = () => {
     return res.data.results.map(_transformCharacter);
   }
 
+  const getCharacterByName = async (name) => {
+    const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+    return res.data.results.map(_transformCharacter);
+  }
+
   const getAllComics = async (offset = 0) => {
     const res = await request(
       `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
@@ -63,6 +68,7 @@ const useMarvelService = () => {
     error,
     getAllCharacters,
     getCharacter,
+    getCharacterByName,
     getAllComics,
     getComic,
     clearError
